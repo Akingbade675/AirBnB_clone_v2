55706ef1d6ab887a6436a4636590e77fe3bd3ae9
Akingbade675 <akingbadebasit26@gmail.com>
2023-02-10 18:01:20 +0000

    bugs still exixts

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,35 +1,40 @@
 #!/usr/bin/python3
-'''starts a Flask web application'''
-
-from flask import Flask, render_template
+"""Starts a Flask web application.
+The application listens on 0.0.0.0, port 5000.
+Routes:
+    /states: HTML page with a list of all State objects.
+    /states/<id>: HTML page displaying the given state with <id>.
+"""
 from models import storage
+from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 
-@app.route('/states', strict_slashes=False)
+@app.route("/states", strict_slashes=False)
 def states():
-    '''Displays an HTML page with a list of all State objects in DBStorage.
+    """Displays an HTML page with a list of all States.
     States are sorted by name.
-    '''
+    """
     states = storage.all("State")
-    return render_template('9-states.html', states=states)
+    return render_template("9-states.html", state=states)
 
 
-@app.route('/states/<id>', strict_slashes=False)
+@app.route("/states/<id>", strict_slashes=False)
 def states_id(id):
-    '''Displays an HTML page with info about <id>, if it exists.'''
+    """Displays an HTML page with info about <id>, if it exists."""
     for state in storage.all("State").values():
         if state.id == id:
-            return render_template('9-states.html', states=state)
-    return render_template('9-states.html')
+            return render_template("9-states.html", state=state)
+    return render_template("9-states.html")
 
 
 @app.teardown_appcontext
 def teardown(exc):
-    '''remove the current SQLAlchemy session'''
+    """Remove the current SQLAlchemy session."""
     storage.close()
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0")
