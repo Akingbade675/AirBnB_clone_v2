95879222a0905b3a00b9cf330be797b5fb14976d
Akingbade675 <akingbadebasit26@gmail.com>
2023-02-13 18:48:10 +0000

    import the State class for querying

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -3,6 +3,7 @@
 
 from flask import Flask, render_template
 from models import storage
+from models.state import State
 
 app = Flask(__name__)
 
@@ -12,7 +13,7 @@ def list_states():
     '''Displays an HTML page with a list of all State objects in DBStorage.
     States are sorted by name.
     '''
-    states = storage.all("State")
+    states = storage.all(State)
     return render_template('7-states_list.html', states=states)
 
 
